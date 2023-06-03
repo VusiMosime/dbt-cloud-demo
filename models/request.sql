9fcf825d08ddbfe34f09a2275fb11daf0fd21a94
Vusi <vusi.mosime@gmail.com>
2023-06-03 19:14:23 +0000

    test models

--- models/request.sql
+++ models/request.sql
@@ -1,8 +1,24 @@
-with source as(
-
-select
-*
-from raw.jaffle_shop.orders
+WITH RECURSIVE employee_chain AS (
+  SELECT
+    ID,
+    FIRSTNAME,
+    LASTNAME,
+    FIRSTNAME || ' ' || LASTNAME AS chain
+  FROM ANALYTICS.EMPLOYEE 
+  WHERE manager_id IS NULL
+  UNION ALL
+  SELECT
+     m.id,
+    m.FIRSTNAME,
+    m.LASTNAME,
+    chain || '->' || m.LASTNAME || ' ' || m.LASTNAME
+  FROM employee_chain
+  JOIN ANALYTICS.EMPLOYEE m
+    ON m.manager_id = employee_chain.id
 )
-select * from source
-
+ 
+SELECT
+  FirstName,
+  LastName,
+  chain
+FROM employee_chain;
